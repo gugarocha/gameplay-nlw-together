df1bbfe29a055b10eb29d28ea2989fc3ab36c928
Gustavo <guga.rocha.silveira@hotmail.com>
2021-06-27 18:00:28 -0300

    add border to SmallInput and TextArea components

--- src/components/SmallInput/styles.ts
+++ src/components/SmallInput/styles.ts
@@ -8,6 +8,8 @@ export const styles = StyleSheet.create({
     height: 48,
     marginRight: 4,
     borderRadius: 8,
+    borderWidth: 1,
+    borderColor: theme.colors.secondary50,
     backgroundColor: theme.colors.secondary40,
     color: theme.colors.heading,
     fontFamily: theme.fonts.text400,
--- src/components/TextArea/styles.ts
+++ src/components/TextArea/styles.ts
@@ -8,6 +8,8 @@ export const styles = StyleSheet.create({
     height: 95,
     marginRight: 4,
     borderRadius: 8,
+    borderWidth: 1,
+    borderColor: theme.colors.secondary50,
     backgroundColor: theme.colors.secondary40,
     color: theme.colors.heading,
     fontFamily: theme.fonts.text400,
