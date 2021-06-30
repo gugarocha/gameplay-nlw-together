3ffce7ca57068e9ba6e3f47b856932df98bf20b7
Gustavo <guga.rocha.silveira@hotmail.com>
2021-06-30 20:45:58 -0300

    make refactors

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -7,15 +7,13 @@ import React, {
 
 import * as AuthSession from 'expo-auth-session';
 
-import {
-  REDIRECT_URI,
-  SCOPE,
-  RESPONSE_TYPE,
-  CLIENT_ID,
-  CDN_IMAGE
-} from '../configs';
-
 import { api } from '../services/api';
+
+const { REDIRECT_URI } = process.env;
+const { SCOPE } = process.env;
+const { RESPONSE_TYPE } = process.env;
+const { CLIENT_ID } = process.env;
+const { CDN_IMAGE } = process.env;
 
 type User = {
   id: string;
@@ -38,7 +36,8 @@ type AuthProviderProps = {
 
 type AuthorizationResponse = AuthSession.AuthSessionResult & {
   params: {
-    access_token: string;
+    access_token?: string;
+    error?: string;
   }
 }
 
@@ -57,7 +56,7 @@ function AuthProvider({ children }: AuthProviderProps) {
       const { type, params } = await AuthSession
         .startAsync({ authUrl }) as AuthorizationResponse;
 
-      if (type === 'success') {
+      if (type === 'success' && !params.error) {
         api.defaults.headers.authorization = `Bearer ${params.access_token}`;
 
         const userInfo = await api.get('/users/@me');
@@ -69,16 +68,13 @@ function AuthProvider({ children }: AuthProviderProps) {
           ...userInfo.data,
           firstName,
           token: params.access_token
-        })
-
-        setLoading(false);
-      } else {
-        setLoading(false);
+        });
       };
-
     } catch {
       throw new Error('Não foi possível autenticar');
-    };
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
