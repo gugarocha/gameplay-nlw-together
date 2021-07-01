92993886d6cbb621863f5a773c1672b4655f7394
Gustavo <guga.rocha.silveira@hotmail.com>
2021-06-30 21:11:14 -0300

    persist user data in AsyncStorage

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -2,12 +2,16 @@ import React, {
   ReactNode,
   createContext,
   useContext,
-  useState
+  useState,
+  useEffect
 } from 'react';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import * as AuthSession from 'expo-auth-session';
 
 import { api } from '../services/api';
+import { COLLECTION_USERS } from '../configs/database';
 
 const { REDIRECT_URI } = process.env;
 const { SCOPE } = process.env;
@@ -64,18 +68,37 @@ function AuthProvider({ children }: AuthProviderProps) {
         const firstName = userInfo.data.username.split(' ')[0];
         userInfo.data.avatar = `${CDN_IMAGE}/avatars/${userInfo.data.id}/${userInfo.data.avatar}.png`
 
-        setUser({
+        const userData = {
           ...userInfo.data,
           firstName,
           token: params.access_token
-        });
+        };
+
+        await AsyncStorage.setItem(COLLECTION_USERS, JSON.stringify(userData));
+        setUser(userData);
       };
     } catch {
       throw new Error('Não foi possível autenticar');
     } finally {
       setLoading(false);
-    }
+    };
   };
+
+  async function loadUserStorageData() {
+    const storage = await AsyncStorage.getItem(COLLECTION_USERS);
+
+    if(storage) {
+      const userLogged = JSON.parse(storage) as User;
+
+      api.defaults.headers.authorization = `Bearer ${userLogged.token}`;
+
+      setUser(userLogged);
+    };
+  };
+
+  useEffect(() => {
+    loadUserStorageData();
+  }, []);
 
   return (
     <AuthContext.Provider value={{
